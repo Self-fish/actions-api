0e6a863a09cb11a639b6f4b61cdd962fb9212008
pablo <pablo@pablos-MacBook-Pro.local>
2021-03-23 20:58:23 +0100

    Adding action for turning off the lights

--- src/main/java/org/selffish/domain/entities/CustomAction.kt
+++ src/main/java/org/selffish/domain/entities/CustomAction.kt
@@ -4,11 +4,13 @@ package org.selffish.domain.entities
 data class CustomAction(val action: ActionType, val step: Step)
 
 enum class ActionType {
-    CLEAN_AQUARIUM
+    CLEAN_AQUARIUM,
+    LIGHT_CONTROL
 }
 
 enum class Step {
     EMPTY_AQUARIUM,
-    FILL_AQUARIUM
+    FILL_AQUARIUM,
+    TURN_OFF_LIGHT
 }
 
